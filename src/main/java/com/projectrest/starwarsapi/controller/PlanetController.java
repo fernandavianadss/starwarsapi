9230e1ad3f2887f644a2f42dd1f759ae670a526d
fernandavianadss <fernanda.vianadss@gmail.com>
2021-01-14 11:12:46 -0300

    Developed planet get operation

--- src/main/java/com/projectrest/starwarsapi/controller/PlanetController.java
+++ src/main/java/com/projectrest/starwarsapi/controller/PlanetController.java
@@ -1,7 +1,10 @@
 package com.projectrest.starwarsapi.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.projectrest.starwarsapi.dto.request.PlanetDTO;
 import com.projectrest.starwarsapi.dto.response.MessageResponseDTO;
+import com.projectrest.starwarsapi.entity.Planet;
 import com.projectrest.starwarsapi.service.PlanetService;
 
 import lombok.AllArgsConstructor;
@@ -25,5 +29,10 @@ public class PlanetController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public MessageResponseDTO createPlanet(@RequestBody PlanetDTO planetDTO) {
 		return planetService.createPlanet(planetDTO);
+	}
+	
+	@GetMapping
+	public List<Planet> listAll() {
+		return planetService.listAll();
 	}
 }
--- src/main/java/com/projectrest/starwarsapi/service/PlanetService.java
+++ src/main/java/com/projectrest/starwarsapi/service/PlanetService.java
@@ -1,5 +1,7 @@
 package com.projectrest.starwarsapi.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -35,6 +37,10 @@ public class PlanetService {
                 .builder()
                 .message(message + id)
                 .build();
+	}
+
+	public List<Planet> listAll() {
+		return planetRepository.findAll();
 	}
 
 }
